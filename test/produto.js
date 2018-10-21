a6c6f5012b4f3726a091042d133981c90cf363bd
Eduardo <dev_eduardo@hotmail.com>
2018-10-21 20:58:26 -0300

    dando truncante no banco

--- test/produto.js
+++ test/produto.js
@@ -1,8 +1,28 @@
 const app = require('../config/server')();
 const request = require('supertest');
+const mysql = require('mysql2/promise');
 
 describe('#ProdutoController', () => {
 
+
+    beforeEach((done) => {
+        (async () => {
+
+            try {
+                const conn = await mysql.createConnection(global.config);
+
+                await conn.query('truncate livros');
+
+                conn.end();
+
+                done();
+
+            } catch (error) {
+                console.log(error);
+            }
+        })()
+
+    })
     it('#Lista produtos com json', (done) => {
         request(app)
             .get('/produtos')
@@ -10,12 +30,12 @@ describe('#ProdutoController', () => {
             .expect('Content-Type', /json/)
             .expect(200, done);
     });
-    
+
     it('#cadastro droduto sem dados html', (done) => {
         request(app)
             .post('/produtos')
             .set('Accept', 'application/json')
-            .send( { titulo : "", descricao : "", preco: ""})
+            .send({ titulo: "", descricao: "", preco: "" })
             .expect(400, done);
     });
 
@@ -23,14 +43,14 @@ describe('#ProdutoController', () => {
         request(app)
             .post('/produtos')
             .set('Accept', 'application/json')
-            .send( { titulo : "teste", descricao : "kkkk", preco: 99.99})
+            .send({ titulo: "teste", descricao: "kkkk", preco: 99.99 })
             .expect(200, done);
     });
 
     it('#cadastro droduto com  html', (done) => {
         request(app)
             .post('/produtos')
-            .send( { titulo : "Eduardo", descricao : "teste eduardo", preco: 99.90})
+            .send({ titulo: "Eduardo", descricao: "teste eduardo", preco: 99.90 })
             .expect(302, done);
     });
 });
