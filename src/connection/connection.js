30316fa592a6d6ca1a3fb5c24cd06385b857fd73
Eduardo <dev_eduardo@hotmail.com>
2018-10-27 19:00:33 -0300

    atualizando

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -4,10 +4,10 @@ let config;
 if (process.env.NODE_ENV == 'production') {
     
     config = {
-        host: 'localhost',
-        user: 'root',
-        password: 'edu123',
-        database: 'casa_do_codigo'
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
     }
 }
 else {
