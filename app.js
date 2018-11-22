ad2c38853cc4002701fe837dd7f2c133d72cb5aa
Eduardo <dev_eduardo@hotmail.com>
2018-11-22 19:38:32 -0200

    atualizando dados

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const io = require('socket.io')(http);
 
 app.set('io', io);
 
+process.env.PORT = 8080;
+
 http.listen(process.env.PORT, () => {
-    console.log('servidor rodando');
+    console.log(`servidor rodando ${process.env.PORT}`);
 });
--- src/connection/connection.js
+++ src/connection/connection.js
@@ -3,11 +3,18 @@ let config;
 
 if (process.env.NODE_ENV == 'production') {
     
-    config = {
+    /* config = {
         host: process.env.HOST,
         user: process.env.USER,
         password: process.env.PASSWORD,
         database: process.env.DATABASE
+    } */
+
+    config = {
+        host: 'localhost',
+        user: 'root',
+        password: 'edu123',
+        database: 'casa_do_codigo_teste'
     }
 }
 else {
